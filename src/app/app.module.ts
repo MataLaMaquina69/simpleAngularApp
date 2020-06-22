871c2156dc415ef98ccd307ad437016d7a83acba
Mata-Alejandro <alejandroavila98@hotmail.com>
2020-06-22 10:30:05 -0500

    project base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EncabezadoComponent
   ],
   imports: [
     BrowserModule,
